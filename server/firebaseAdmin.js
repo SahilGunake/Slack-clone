10bf1ba2dcc6fabf602d973defaa4a1ab2a40f21
Sahil Gunake <sahilgunake@gmail.com>
2025-03-01 19:04:30 +0530

    Firebase secret configured

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,6 +1,15 @@
 import admin from 'firebase-admin';
 import serviceAccount from './serviceAccountKey.json' with { type: "json" };
 
+const config = {
+  credential: admin.credential.cert({
+    projectId: process.env["FIREBASE_ADMIN_PROJECT_ID"],
+    clientEmail: process.env["FIREBASE_ADMIN_CLIENT_EMAIL"],
+    privateKey: process.env["FIREBASE_ADMIN_PRIVATE_KEY"].replace(/\\n/g, "\n"),
+  }),
+  // databaseURL: process.env.SVELTE_APP_FIREBASE_DATABASE_URL,
+};
+
 
 // Initialize Firebase Admin
 if (!admin.apps.length) {
