505b1b8cefae0779c36a40149f8903488b6c1f2c
Sahil Gunake <sahilgunake@gmail.com>
2025-03-01 19:33:23 +0530

    Updated firebaseAdmin and vite config

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,10 +1,11 @@
 import admin from 'firebase-admin';
+// import serviceAccount from './serviceAccountKey.json' with { type: "json" };
 
 const config = {
   credential: admin.credential.cert({
-    projectId: process.env["FIREBASE_ADMIN_PROJECT_ID"],
-    clientEmail: process.env["FIREBASE_ADMIN_CLIENT_EMAIL"],
-    privateKey: process.env["FIREBASE_ADMIN_PRIVATE_KEY"].replace(/\\n/g, "\n"),
+    projectId: import.meta.env["FIREBASE_ADMIN_PROJECT_ID"],
+    clientEmail: import.meta.env["FIREBASE_ADMIN_CLIENT_EMAIL"],
+    privateKey: import.meta.env["FIREBASE_ADMIN_PRIVATE_KEY"].replace(/\\n/g, "\n"),
   }),
   // databaseURL: process.env.SVELTE_APP_FIREBASE_DATABASE_URL,
 };
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const env = loadEnv(mode, process.cwd(), '');
-
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    define: {
-      'process.env.SOME_KEY': JSON.stringify(env.SOME_KEY)
-    },
-    plugins: [react()],
-  }
+export default defineConfig({
+  plugins: [react()],
 })
